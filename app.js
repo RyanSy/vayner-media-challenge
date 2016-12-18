543489f5d854ce5098f2c1d28a602747a2158c3b
Ryan Sy <swerve@MacBook-Pro.fios-router.home>
2016-12-18 14:31:15 -0500

    steps 1-3 done

--- app.js
+++ app.js
@@ -1,82 +1,66 @@
+// API url
+var root = 'https://jsonplaceholder.typicode.com';
+
+// create function to get users
+var getUser = function(id) {
+  $.ajax({
+    url: root + '/users',
+    method: 'get',
+    data: {
+      "id": id
+    }
+  }).then(function(data) {
+    // create table div for user
+    $('#userTables').append('<h3 class="tableTitle">' + data[0].name + '</h3><div id=' + id + ' ondrop="drop(event)" ondragover="allowDrop(event)"></div><br>');
+  });
+}
+
+// create function to get albums
+var getAlbums = function(userId) {
+  $.ajax({
+    url: root + '/albums',
+    method: 'get',
+    data: {
+      "userId": userId
+    }
+  }).then(function(data) {
+    // append albums to user tables
+    for (var i = 0; i < data.length; i++) {
+      $('#' + userId).append('<div id="album' + data[i].id + '" class="tableItem" draggable="true" ondragstart="drag(event)"><p>' + data[i].title + '</p></div>');
+    }
+  });
+}
+
+// implement drag and drop
 function allowDrop(ev) {
-
   ev.preventDefault();
-
 }
 
 function drag(ev) {
-
   ev.dataTransfer.setData("text", ev.target.id);
-  console.log(ev.target.id);
-
 }
 
 function drop(ev) {
-
   ev.preventDefault();
-
   var data = ev.dataTransfer.getData("text");
-
   ev.target.appendChild(document.getElementById(data));
-
-  console.log(ev);
-
-  // $.ajax('http://jsonplaceholder.typicode.com/albums/' + data, {
-  //   method: 'PATCH',
-  //   data: {
-  //     userId:
-  //   }
-  // }).then(function(data) {
-  //   console.log(data);
-  // });
-
+  // update 'userId' property on the album
+  var albumId = data.slice(5);
+  var newUserId = ev.path[0].id;
+  $.ajax('http://jsonplaceholder.typicode.com/albums/' + albumId, {
+    method: 'PATCH',
+    data: {
+      userId: newUserId
+    }
+  }).then(function(data) {
+    console.log('patch =>', data);
+  });
 }
 
-var root = 'https://jsonplaceholder.typicode.com';
+// get users 1 & 2
+getUser(1);
+getUser(2);
 
-// get and display names of user 1 & user 2
-$.ajax({
-  url: root + '/users',
-  method: 'GET'
-}).then(function(data) {
-
-  var user1 = $.grep(data, function(e) {
-    return e.id == 1;
-  });
-
-  var user2 = $.grep(data, function(e) {
-    return e.id == 2;
-  });
-
-  var users = [];
-
-  users.push(user1[0], user2[0]);
-
-  $('#user1').append('<div class="tableTitle"><h3>' + users[0].name + '</h3><span class="hidden">' + users[0].id + '</span></div>');
-  $('#user2').append('<div class="tableTitle"><h3>' + users[1].name + '</h3><span class="hidden">' + users[1].id + '</span></div>');
-
-});
-
-// get and display albums of user 1 & user 2
-$.ajax({
-  url: root + '/albums',
-  method: 'GET'
-}).then(function(data) {
-
-  var user1albums = $.grep(data, function(e) {
-    return e.userId == 1;
-  });
-
-  var user2albums = $.grep(data, function(e) {
-    return e.userId == 2;
-  });
-
-  for (i = 0; i < user1albums.length; i++) {
-    $('#user1').append('<div id="' + user1albums[i].id + '" class="tableItem" draggable="true" ondragstart="drag(event)"><span>' + user1albums[i].id + '</span> <span>' + user1albums[i].title + '</span>');
-  }
-
-  for (i = 0; i < user2albums.length; i++) {
-    $('#user2').append('<div id="' + user2albums[i].id + '" class="tableItem" draggable="true" ondragstart="drag(event)"><span>' + user2albums[i].id + '</span> <span>' + user2albums[i].title + '</span>');
-  }
-
-});
+// get users' 1 & 2 albums
+getAlbums(1);
+getAlbums(2);
