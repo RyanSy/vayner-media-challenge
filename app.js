dec5e6316df34471efafbbe8f7120bcef1a26013
Ryan Sy <swerve@MacBook-Pro.local>
2016-12-20 12:10:08 -0500

    done

--- app.js
+++ app.js
@@ -1,84 +1,83 @@
-// $(document).ready(function() {
-  // users to get
-  var users = [1, 2];
+// users to get
+var users = [1, 2];
 
-  // API url
-  var root = 'https://jsonplaceholder.typicode.com';
+// API url
+var root = 'https://jsonplaceholder.typicode.com';
 
-  // create function to get users
-  var getUser = function(id) {
-    $.ajax({
-      url: root + '/users',
-      method: 'get',
-      data: {
-        "id": id
-      }
-    }).then(function(data) {
-      console.log('getUser() called => ', data);
-      // create table div for user
-        $('#user-tables').append(
-        '<div id=' + id + ' ondragover="allowDrop(event)" ondrop="drop(event)" class="table-wrapper"><div class="table-name">'
-        + data[0].name + '</div><div class="table-heading"><div class="album-id">ID</div><div class="album-title">Title</div></div><div id="table' + id + '" class="table-body"></div></div>');
-    });
-  }
-
-  // create function to get albums
-  var getAlbums = function(userId) {
+// function to create tables with user and album data
+function createTables(id) {
+  $.ajax({
+    url: root + '/users',
+    method: 'get',
+    data: {
+      "id": id
+    }
+  }).then(function(data) {
+    // console.log('* GET root/users/' + id + ' ajax call => ', data);
+    // create table div for user
+    $('#user-tables').append('<div class="table-wrapper"><div class="table-name">' + data[0].name + '</div><div class="table-heading"><div class="album-id">ID</div><div class="album-title">Title</div></div><div id="table' + data[0].id + '" class="table-body" ondragover="allowDrop(event)" ondrop="drop(event)"></div></div>');
+  }).then(function() {
     $.ajax({
       url: root + '/albums',
       method: 'get',
       data: {
-        "userId": userId
+        "userId": id
       }
     }).then(function(data) {
-      console.log('getAlbums() called => ', data);
-      // append albums to user tables
+      // console.log('* GET root/albums?userId=' + id + ' ajax call => ', data);
+      // append each album to respective user tables
       for (var i = 0; i < data.length; i++) {
-        $('#table' + userId).append('<div id="album' + data[i].id + '" class="table-row" draggable="true" ondragstart="drag(event)"><div class="album-id">' + data[i].id + '</div><div class="album-title">' + data[i].title + '</div></div>');
+        $('#table' + data[i].userId).append('<div id="album' + data[i].id + '" class="table-row" draggable="true" ondragstart="drag(event)"><div class="album-id">' + data[i].id + '</div><div class="album-title">' + data[i].title + '</div></div>');
       }
     });
-  }
-
-  // implement drag and drop
-  function allowDrop(ev) {
-    ev.preventDefault();
-  }
-
-  function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-  }
-
-  function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-    console.log('ev.target => ', ev.target);
-    console.log('data => ', data);
-    // update 'userId' property on the album when dropped onto another table
-    var albumId = data.slice(5);
-    console.log('ev.path[2].id', ev.path[2].id);
-    var newUserId = (ev.path[2].id);
-    console.log('newUserId => ', newUserId);
-    $.ajax('http://jsonplaceholder.typicode.com/albums/' + albumId, {
-      method: 'patch',
-      data: {
-        userId: newUserId
-      }
-    }).then(function(data2) {
-      console.log('data2', data2);
-      console.log('drop(ev) called =>', data2.userId);
-      getAlbums(data2.userId)
-    });
-  }
-
-// get users and albums
-for (var i = 0; i < users.length; i++) {
-  // console.log('users[i] => ', users[i]);
-  getUser(users[i]);
-  console.log('getUser called ' + (i + 1) + ' time(s)');
-  getAlbums(users[i]);
-  console.log('getAlbums called ' + (i + 1) + ' time(s)');
-
+  })
 }
 
-// }); // end document ready
+// implement drag and drop
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  // console.log('* drop(ev) called');
+  console.log('ev =>', ev);
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+  /* update 'userId' property on the album when dropped onto another table
+      when album moved from table 1 to table 2, ev.path.length == 10;
+      when album moved from table 1 to table 2, ev.path.length == 10;
+      could not figure out why */
+  if (ev.path.length == 10) {
+    var newUserId = (ev.path[2].id).slice(5);
+  } else if (ev.path.length == 9) {
+    var newUserId = (ev.path[1].id).slice(5);
+  }
+  // console.log('newUserId => ', newUserId );
+  $('#' + data).hide();
+  var albumId = data.slice(5);
+  $.ajax({
+    url: root + '/albums/' + albumId,
+    method: 'patch',
+    data: {
+      userId: newUserId
+    }
+  }).then(function(data) {
+    // console.log('* PATCH root/albums/' + albumId + '?userId=' + newUserId, data);
+    $('#table' + data.userId).append('<div id="album' + data.id + '" class="table-row" draggable="true" ondragstart="drag(event)"><div class="album-id">' + data.id + '</div><div class="album-title">' + data.title + '</div></div>');
+    alert('Album "' + data.title + '" successfully moved.')
+  });
+}
+
+// call createTables() for each user
+function run() {
+  for (var i = 0; i < users.length; i++) {
+    createTables(users[i]);
+  }
+}
+
+run();
